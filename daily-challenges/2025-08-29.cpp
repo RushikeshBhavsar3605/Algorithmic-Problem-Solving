56fc5be7209162ae8a1b5a875be7b1712a1e86b4
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-08-29 10:46:36 +0530

    [Day 29] Solved: Alice and Bob Playing Flower Game

---
+++ daily-challenges/2025-08-29.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Math): */
+class Solution {
+  public:
+    long long flowerGame(int n, int m) {
+        ll count = 0;
+
+        int oddN = (n + 1) / 2;
+        int evenM = m / 2;
+        count += (1LL * oddN * evenM);
+
+        int evenN = n / 2;
+        int oddM = (m + 1) / 2;
+        count += (1LL * evenN * oddM);
+
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.flowerGame(3, 2) << endl;
+    cout << obj.flowerGame(1, 1) << endl;
+}
+
+/*
+Problem: Alice and Bob Playing Flower Game
+*/
