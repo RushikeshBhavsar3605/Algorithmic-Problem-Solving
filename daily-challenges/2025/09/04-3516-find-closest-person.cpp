42168f49e5d9e982936d2926b83030c139d3f93a
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-04 09:35:47 +0530

    [Day 4] Solved: Find Closest Person

---
+++ daily-challenges/2025/09/04-3516-find-closest-person.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    int findClosest(int x, int y, int z) {
+        int time1 = abs(x - z);
+        int time2 = abs(y - z);
+
+        if (time1 == time2)
+            return 0;
+
+        if (time1 < time2)
+            return 1;
+
+        return 2;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.findClosest(2, 7, 4) << endl;
+    cout << obj.findClosest(2, 5, 6) << endl;
+    cout << obj.findClosest(1, 5, 3) << endl;
+}
+
+/*
+Problem: Find Closest Person
+*/
