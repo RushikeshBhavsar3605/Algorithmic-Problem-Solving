984446cec935665924b91dea40b421c6b365694a
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-05 11:35:43 +0530

    [Day 5] Solved: Minimum Operations to Make the Integer Zero

---
+++ daily-challenges/2025/09/05-2749-minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Bit Manipulation): */
+class Solution {
+  public:
+    int makeTheIntegerZero(int num1, int num2) {
+        loop(i, 0, 61) {
+            ll rem = num1 - ((ll)i) * num2;
+            if (rem < i)
+                return -1;
+            if (i >= __builtin_popcountll(rem))
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.makeTheIntegerZero(3, -2) << endl;
+    cout << obj.makeTheIntegerZero(5, 7) << endl;
+}
+
+/*
+Problem: Minimum Operations to Make the Integer Zero
+*/
