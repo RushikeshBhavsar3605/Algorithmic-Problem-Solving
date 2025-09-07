7bfbf4c791da54fcb708003fe5fedf6347427805
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-07 19:09:49 +0530

    [Day 7] Solved: Find N Unique Integers Sum up to Zero

---
+++ daily-challenges/2025/09/07-1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Observation): */
+class Solution {
+  public:
+    vector<int> sumZero(int n) {
+        vi result;
+        revloop(i, (n >> 1), 1)
+            result.pb(-i);
+
+        if (n & 1)
+            result.pb(0);
+
+        loop(i, 1, (n >> 1) + 1)
+            result.pb(i);
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    dump(obj.sumZero(5));
+    dump(obj.sumZero(3));
+    dump(obj.sumZero(1));
+}
+
+/*
+Problem: Find N Unique Integers Sum up to Zero
+*/
