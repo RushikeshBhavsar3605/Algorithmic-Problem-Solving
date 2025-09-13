8feff2d731efe370984b39bed251bb3ed5b90cba
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-13 10:17:04 +0530

    [Day 13] Solved: Find Most Frequent Vowel and Consonant

---
+++ daily-challenges/2025/09/13-3541-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    int maxFreqSum(string s) {
+        unordered_set<char> st = {'a', 'e', 'i', 'o', 'u'};
+        umap<int, int> count;
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        iterate(it, s) {
+            count[it]++;
+            if (st.count(it))
+                updateMax(vowelCount, count[it]);
+            else
+                updateMax(consonantCount, count[it]);
+        }
+
+        return vowelCount + consonantCount;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.maxFreqSum("successes") << endl;
+    cout << obj.maxFreqSum("aeiaeia") << endl;
+}
+
+/*
+Problem: Find Most Frequent Vowel and Consonant
+*/
