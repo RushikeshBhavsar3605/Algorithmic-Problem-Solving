d3884d25ca6bb9a8a5835299ba29d496b94ee16a
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-22 19:41:49 +0530

    [Day 22] Solved: Count Elements With Maximum Frequency

---
+++ daily-challenges/2025/09/22-3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    int maxFrequencyElements(vector<int> &nums) {
+        umap<int, int> mp;
+        int maxFreq = 0;
+        iterate(it, nums)
+            updateMax(maxFreq, ++mp[it]);
+
+        int totalFreq = 0;
+        iterate(it, mp)
+            if (it.ss == maxFreq)
+                totalFreq += it.ss;
+
+        return totalFreq;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 2, 2, 3, 1, 4};
+    cout << obj.maxFrequencyElements(v) << endl;
+    v = {1, 2, 3, 4, 5};
+    cout << obj.maxFrequencyElements(v) << endl;
+}
+
+/*
+Problem: Count Elements With Maximum Frequency
+*/
