6b2b5f21de8946e3f97e5c7ede93508ce8495f5b
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-09-28 13:28:03 +0530

    [Day 28] Solved: Largest Perimeter Triangle

---
+++ daily-challenges/2025/09/28-0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    int largestPerimeter(vector<int> &nums) {
+        sort(all(nums), greater<int>());
+
+        loop(i, 0, sz(nums) - 2)
+            if (nums[i] < nums[i + 1] + nums[i + 2])
+                return nums[i] + nums[i + 1] + nums[i + 2];
+
+        return 0;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {2, 1, 2};
+    cout << obj.largestPerimeter(v) << endl;
+    v = {1, 2, 1, 10};
+    cout << obj.largestPerimeter(v) << endl;
+}
+
+/*
+Problem: Largest Perimeter Triangle
+*/
