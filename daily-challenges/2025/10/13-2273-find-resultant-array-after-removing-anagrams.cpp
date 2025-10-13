36da94906847d9d480cee56306722e09c9113905
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-13 16:50:28 +0530

    [Day 13] Solved: Find Resultant Array After Removing Anagrams

---
+++ daily-challenges/2025/10/13-2273-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  private:
+    bool anagrams(string word1, string word2) {
+        sort(all(word1));
+        sort(all(word2));
+
+        return word1 == word2;
+    }
+
+  public:
+    vector<string> removeAnagrams(vector<string> &words) {
+        vector<string> result = words;
+        int n = sz(result);
+        int i = 1;
+
+        while (i < n) {
+            if (anagrams(result[i - 1], result[i]))
+                result.erase(begin(result) + i);
+            else
+                i++;
+
+            n = sz(result);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<string> v = {"abba", "baba", "bbaa", "cd", "cd"};
+    dump(obj.removeAnagrams(v));
+    v = {"a", "b", "c", "d", "e"};
+    dump(obj.removeAnagrams(v));
+}
+
+/*
+Problem: Find Resultant Array After Removing Anagrams
+*/
