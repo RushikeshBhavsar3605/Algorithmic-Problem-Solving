213a4e5c81f5386709e07ce033c2962b21426591
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-15 18:45:44 +0530

    [Day 15] Solved: Adjacent Increasing Subarrays Detection II

---
+++ daily-challenges/2025/10/15-3350-adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Counting): */
+class Solution {
+  public:
+    int maxIncreasingSubarrays(vector<int> &nums) {
+        int n = sz(nums);
+
+        int result = 0;
+        int cnt = 1, prevCnt = 0;
+
+        loop(i, 1, n) {
+            if (nums[i] > nums[i - 1])
+                cnt++;
+            else {
+                prevCnt = cnt;
+                cnt = 1;
+            }
+
+            updateMax(result, min(cnt, prevCnt));
+            updateMax(result, cnt >> 1);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {2, 5, 7, 8, 9, 2, 3, 4, 3, 1};
+    println(obj.maxIncreasingSubarrays(v));
+    v = {1, 2, 3, 4, 4, 4, 4, 5, 6, 7};
+    println(obj.maxIncreasingSubarrays(v));
+}
+
+/*
+Problem: Adjacent Increasing Subarrays Detection II
+*/
