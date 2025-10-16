40fdfefacfd3811f5ee7331ceb54c00b69e9207e
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-16 16:41:51 +0530

    [Day 16] Solved: Smallest Missing Non-negative Integer After Operations

---
+++ daily-challenges/2025/10/16-2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Hashing): */
+class Solution {
+  public:
+    int findSmallestInteger(vector<int> &nums, int value) {
+        int n = sz(nums);
+        umap<int, int> mp;
+
+        iterate(num, nums)
+            mp[((num % value) + value) % value]++;
+
+        int MEX = 0;
+        while (mp[MEX % value] > 0)
+            mp[MEX++ % value]--;
+
+        return MEX;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, -10, 7, 13, 6, 8};
+    println(obj.findSmallestInteger(v, 5));
+    println(obj.findSmallestInteger(v, 7));
+}
+
+/*
+Problem: Smallest Missing Non-negative Integer After Operations
+*/
