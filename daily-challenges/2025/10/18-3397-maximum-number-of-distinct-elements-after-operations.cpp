9b16584b877cc6c329cfd5df6253e7fbd2b80418
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-18 15:54:07 +0530

    [Day 18] Solved: Maximum Number of Distinct Elements After Operations

---
+++ daily-challenges/2025/10/18-3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Greedy): */
+class Solution {
+  public:
+    int maxDistinctElements(vector<int> &nums, int k) {
+        sort(all(nums));
+        ll last = LLONG_MIN;
+        int result = 0;
+
+        iterate(it, nums) {
+            ll lowerBound = it - k;
+            ll upperBound = it + k;
+            ll target = max(last + 1, lowerBound);
+
+            if (target <= upperBound) {
+                result++;
+                last = target;
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 2, 2, 3, 3, 4};
+    println(obj.maxDistinctElements(v, 2));
+    v = {4, 4, 4, 4};
+    println(obj.maxDistinctElements(v, 1));
+}
+
+/*
+Problem: Maximum Number of Distinct Elements After Operations
+*/
