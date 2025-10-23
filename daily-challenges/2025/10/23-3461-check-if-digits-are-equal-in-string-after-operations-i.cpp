97b6b6a985e964b1db3d69035e0c5f0219863529
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-23 20:15:41 +0530

    [Day 23] Solved: Check If Digits Are Equal in String After Operations I

---
+++ daily-challenges/2025/10/23-3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    bool hasSameDigits(string s) {
+        int n = sz(s) + 1;
+        while (--n != 2)
+            loop(i, 1, n)
+                s[i - 1] = ((s[i] - '0' + s[i - 1] - '0') % 10) + '0';
+
+        return s[0] == s[1] ? true : false;
+    }
+};
+
+int main() {
+    Solution obj;
+    println(obj.hasSameDigits("3902"));
+    println(obj.hasSameDigits("34789"));
+}
+
+/*
+Problem: Check If Digits Are Equal in String After Operations I
+*/
