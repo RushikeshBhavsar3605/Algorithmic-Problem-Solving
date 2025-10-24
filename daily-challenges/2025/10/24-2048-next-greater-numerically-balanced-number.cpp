1da0b9a29200674c3426a3cb83056aea79748ecc
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-24 16:24:43 +0530

    [Day 24] Solved: Next Greater Numerically Balanced Number

---
+++ daily-challenges/2025/10/24-2048-next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  private:
+    bool isBalanced(int n) {
+        vi count(10, 0);
+        while (n != 0) {
+            count[n % 10]++;
+            n /= 10;
+        }
+
+        loop(i, 0, 10)
+            if (count[i] != 0 && count[i] != i)
+                return false;
+
+        return true;
+    }
+
+  public:
+    int nextBeautifulNumber(int n) {
+        loop(i, n + 1, 1224445)
+            if (isBalanced(i))
+                return i;
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    println(obj.nextBeautifulNumber(1));
+    println(obj.nextBeautifulNumber(1000));
+    println(obj.nextBeautifulNumber(3000));
+    println(obj.nextBeautifulNumber(1000000));
+}
+
+/*
+Problem: Next Greater Numerically Balanced Number
+*/
