d62b69b9c7b0156f688a4ed5938bc353c7ffb98c
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-29 11:56:46 +0530

    [Day 29] Solved: Smallest Number With All Set Bits

---
+++ daily-challenges/2025/10/29-3370-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Optimal): */
+class Solution {
+  public:
+    int smallestNumber(int n) {
+        loop(i, 1, 11) {
+            int num = (1 << i) - 1;
+            if (num >= n)
+                return num;
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    println(obj.smallestNumber(5));
+    println(obj.smallestNumber(10));
+    println(obj.smallestNumber(3));
+}
+
+/*
+Problem: Smallest Number With All Set Bits
+*/
