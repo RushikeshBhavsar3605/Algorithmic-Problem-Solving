98b2d3547edb4dbcad545b05a0aea4bfe00d6b76
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-10-30 17:57:10 +0530

    [Day 30] Solved: Minimum Number of Increments on Subarrays to Form a Target Array

---
+++ daily-challenges/2025/10/30-1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Greedy): */
+class Solution {
+  public:
+    int minNumberOperations(vector<int> &target) {
+        int n = sz(target);
+        int result = target[0];
+
+        loop(i, 1, n) {
+            if (target[i - 1] < target[i])
+                result += target[i] - target[i - 1];
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 2, 3, 2, 1};
+    println(obj.minNumberOperations(v));
+    v = {3, 1, 1, 2};
+    println(obj.minNumberOperations(v));
+    v = {3, 1, 5, 4, 2};
+    println(obj.minNumberOperations(v));
+}
+
+/*
+Problem: Minimum Number of Increments on Subarrays to Form a Target Array
+*/
