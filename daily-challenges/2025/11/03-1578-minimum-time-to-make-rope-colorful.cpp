6b72182db2734bfe6d705d4887bf53b9e71dd8a7
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-11-03 19:17:52 +0530

    [Day 3] Solved: Minimum Time to Make Rope Colorful

---
+++ daily-challenges/2025/11/03-1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Greedy): */
+class Solution {
+  public:
+    int minCost(string colors, vector<int> &neededTime) {
+        int n = sz(colors);
+
+        int sum = 0;
+        loop(i, 1, n) {
+            if (colors[i - 1] == colors[i]) {
+                sum += min(neededTime[i], neededTime[i - 1]);
+                updateMax(neededTime[i], neededTime[i - 1]);
+            }
+        }
+
+        return sum;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 2, 3, 4, 5};
+    println(obj.minCost("abaac", v));
+    v = {1, 2, 3};
+    println(obj.minCost("abc", v));
+    v = {1, 2, 3, 4, 1};
+    println(obj.minCost("aabaa", v));
+}
+
+/*
+Problem: Minimum Time to Make Rope Colorful
+*/
