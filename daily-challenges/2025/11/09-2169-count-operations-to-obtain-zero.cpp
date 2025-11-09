7dcf349b01b286900e0161e71e6df568cdaf1183
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-11-09 17:15:53 +0530

    [Day 9] Solved: Count Operations to Obtain Zero

---
+++ daily-challenges/2025/11/09-2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive):
+class Solution {
+  public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+
+            count++;
+        }
+
+        return count;
+    }
+};
+*/
+
+/* Approach 2 (Euclidean Algorithm): */
+class Solution {
+  public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 && num2) {
+            count += num1 / num2;
+            num1 %= num2;
+            swap(num1, num2);
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+    println(obj.countOperations(2, 3));
+    println(obj.countOperations(10, 10));
+}
+
+/*
+Problem: Count Operations to Obtain Zero
+*/
