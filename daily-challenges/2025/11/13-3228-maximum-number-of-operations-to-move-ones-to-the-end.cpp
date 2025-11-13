c234435b5aab8d68c15773d26be974988e15d931
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-11-13 09:50:46 +0530

    [Day 13] Solved: Maximum Number of Operations to Move Ones to the End

---
+++ daily-challenges/2025/11/13-3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Greedy): */
+class Solution {
+  public:
+    int maxOperations(string s) {
+        int result = 0;
+        int oneCount = 0; // Cumulative count of '1's seen so far
+        bool prevZero = false;
+
+        loop(i, 0, sz(s)) {
+            if (s[i] == '1') {
+                // '1' after gap of '0's: all previous '1's jump over this gap
+                if (prevZero)
+                    result += oneCount;
+
+                oneCount++;
+                prevZero = false;
+            } else
+                prevZero = true;
+        }
+
+        // Trailing '0's: all accumulated '1's jump to end
+        if (s[sz(s) - 1] == '0')
+            result += oneCount;
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    println(obj.maxOperations("1001101"));
+    println(obj.maxOperations("00111"));
+}
+
+/*
+Problem: Maximum Number of Operations to Move Ones to the End
+*/
