17a4641b2e823310d6e758e464114133a029c2e3
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-11-17 13:05:44 +0530

    [Day 17] Solved: Check If All 1's Are at Least Length K Places Away

---
+++ daily-challenges/2025/11/17-1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    bool kLengthApart(vector<int> &nums, int k) {
+        int n = sz(nums);
+        int prevOne = -1;
+
+        loop(i, 0, n) {
+            if (nums[i] == 0)
+                continue;
+
+            if (prevOne != -1 && i - prevOne - 1 < k)
+                return false;
+            prevOne = i;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 0, 0, 0, 1, 0, 0, 1};
+    println(obj.kLengthApart(v, 2));
+    v = {1, 0, 0, 1, 0, 1};
+    println(obj.kLengthApart(v, 2));
+}
+
+/*
+Problem: Check If All 1's Are at Least Length K Places Away
+*/
