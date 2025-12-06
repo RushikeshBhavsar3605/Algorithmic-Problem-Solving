534d7a9f7ef4ea58a99647e1ff4952536b239b1f
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-12-06 18:27:53 +0530

    [Day 6] Solved: Count Partitions With Max-Min Difference at Most K

---
+++ daily-challenges/2025/12/06-3578-count-partitions-with-max-min-difference-at-most-k.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Sliding Window + Dynamic Programming): */
+class Solution {
+  public:
+    int countPartitions(vector<int> &nums, int k) {
+        int n = nums.size();
+        int mod = 1e9 + 7;
+        vector<ll> dp(n + 1);
+        vector<ll> prefix(n + 1);
+        multiset<int> cnt;
+        dp[0] = 1;
+        prefix[0] = 1;
+
+        for (int i = 0, j = 0; i < nums.size(); i++) {
+            cnt.emplace(nums[i]);
+            // adjust window
+            while (j <= i && *cnt.rbegin() - *cnt.begin() > k) {
+                cnt.erase(cnt.find(nums[j]));
+                j++;
+            }
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        }
+
+        return dp[n];
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {9, 4, 1, 3, 7};
+    println(obj.countPartitions(v, 4));
+    v = {3, 3, 4};
+    println(obj.countPartitions(v, 0));
+}
+
+/*
+Problem: Count Partitions With Max-Min Difference at Most K
+*/
