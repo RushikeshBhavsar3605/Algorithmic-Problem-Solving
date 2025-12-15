ad1ec694bd8c57b725924b0e63053b3864fe8199
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-12-15 09:31:05 +0530

    [Day 15] Solved: Number of Smooth Descent Periods of a Stock

---
+++ daily-challenges/2025/12/15-2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    long long getDescentPeriods(vector<int> &prices) {
+        int n = sz(prices);
+        ll result = 1;
+
+        int prev = 1;
+
+        loop(i, 1, n) {
+            if (prices[i] == prices[i - 1] - 1)
+                prev++;
+            else
+                prev = 1;
+            result += prev;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {3, 2, 1, 4};
+    println(obj.getDescentPeriods(v));
+    v = {8, 6, 7, 7};
+    println(obj.getDescentPeriods(v));
+    v = {1};
+    println(obj.getDescentPeriods(v));
+}
+
+/*
+Problem: Number of Smooth Descent Periods of a Stock
+*/
