22b87d2d4f63310771218982f469b12a8565b146
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-12-21 22:03:17 +0530

    [Day 21] Solved: Delete Columns to Make Sorted II

---
+++ daily-challenges/2025/12/21-0955-delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Greedy + State Tracking): */
+class Solution {
+  public:
+    int minDeletionSize(vector<string> &strs) {
+        int count = 0;
+        vi sorted(sz(strs) - 1, 0);
+
+        loop(col, 0, sz(strs[0])) {
+            bool flag = false;
+            vi currSorted = sorted;
+
+            loop(row, 1, sz(strs)) {
+                int prev = strs[row - 1][col];
+                int curr = strs[row][col];
+
+                if (prev < curr)
+                    currSorted[row - 1] = 1;
+                if (prev > curr) {
+                    if (currSorted[row - 1] == 1)
+                        continue;
+                    flag = true;
+                    count++;
+                    break;
+                }
+            }
+
+            if (!flag)
+                sorted = currSorted;
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<string> v = {"ca", "bb", "ac"};
+    println(obj.minDeletionSize(v));
+    v = {"xc", "yb", "za"};
+    println(obj.minDeletionSize(v));
+    v = {"zyx", "wvu", "tsr"};
+    println(obj.minDeletionSize(v));
+    v = {"xga", "xfb", "yfa"};
+    println(obj.minDeletionSize(v));
+    v = {"vdy", "vei", "zvc", "zld"};
+    println(obj.minDeletionSize(v));
+}
+
+/*
+Problem: Delete Columns to Make Sorted II
+*/
