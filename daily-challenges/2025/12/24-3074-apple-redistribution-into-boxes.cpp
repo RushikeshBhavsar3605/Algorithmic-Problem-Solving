bfedd7c51464f0159f650bea0e7070c58314123c
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2025-12-24 10:15:19 +0530

    [Day 24] Solved: Apple Redistribution into Boxes

---
+++ daily-challenges/2025/12/24-3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Sorting): */
+class Solution {
+  public:
+    int minimumBoxes(vector<int> &apple, vector<int> &capacity) {
+        int apples = accumulate(all(apple), 0);
+        sort(all(capacity), greater<int>());
+
+        int boxesCount = 0;
+        iterate(it, capacity) {
+            if (apples <= 0)
+                break;
+
+            apples -= it;
+            boxesCount++;
+        }
+
+        return boxesCount;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi a = {1, 3, 2};
+    vi c = {4, 3, 1, 5, 2};
+    println(obj.minimumBoxes(a, c));
+    a = {5, 5, 5};
+    c = {2, 4, 2, 7};
+    println(obj.minimumBoxes(a, c));
+}
+
+/*
+Problem: Apple Redistribution into Boxes
+*/
