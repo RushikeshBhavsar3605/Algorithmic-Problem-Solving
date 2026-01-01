20ce9529fcc8338212980d66d8e51440d201c50c
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-01 11:01:32 +0530

    [Day 1] Solved: Plus One

---
+++ daily-challenges/2026/01/01-0066-plus-one.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    vector<int> plusOne(vector<int> &digits) {
+        int n = sz(digits);
+        vi result;
+        bool change = false;
+
+        revloop(i, n - 1, 0) {
+            if (digits[i] < 9 && !change) {
+                result.pb(digits[i] + 1);
+                change = true;
+            } else if (digits[i] == 9 && !change)
+                result.pb(0);
+            else
+                result.pb(digits[i]);
+        }
+        if (!change)
+            result.pb(1);
+
+        reverse(all(result));
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {1, 2, 3};
+    dump(obj.plusOne(v));
+    v = {4, 3, 2, 1};
+    dump(obj.plusOne(v));
+    v = {9};
+    dump(obj.plusOne(v));
+}
+
+/*
+Problem: Plus One
+*/
