0c7ddbd766f54b33739edf84d9509082cd35fd39
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-06 09:45:30 +0530

    [Day 6] Solved: Maximum Level Sum of a Binary Tree

---
+++ daily-challenges/2026/01/06-1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+class TreeNode {
+  public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int data) {
+        val = data;
+        left = right = nullptr;
+    }
+};
+
+/* Approach 1 (BFS): */
+class Solution {
+  public:
+    int maxLevelSum(TreeNode *root) {
+        queue<TreeNode *> q;
+        q.push(root);
+        pair<int, int> maxLevel = {root->val, 1};
+        int level = 1;
+
+        while (!q.empty()) {
+            int sum = 0;
+            int size = sz(q);
+
+            loop(i, 0, size) {
+                auto node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            if (maxLevel.ff < sum)
+                maxLevel = {sum, level};
+            level++;
+        }
+
+        return maxLevel.ss;
+    }
+};
+
+int main() {
+    Solution obj;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(7);
+    root->right = new TreeNode(0);
+    root->left->left = new TreeNode(7);
+    root->left->right = new TreeNode(-8);
+    println(obj.maxLevelSum(root));
+}
+
+/*
+Problem: Maximum Level Sum of a Binary Tree
+*/
