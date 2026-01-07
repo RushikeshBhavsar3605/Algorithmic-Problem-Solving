2e3c86e77be3373c1c140348373f6842ec116b92
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-07 23:30:14 +0530

    [Day 7] Solved: Maximum Product of Splitted Binary Tree

---
+++ daily-challenges/2026/01/07-1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+class TreeNode {
+  public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int data) {
+        val = data;
+        left = right = nullptr;
+    }
+};
+
+/* Approach 1 (DFS + BFS): */
+class Solution {
+  private:
+    ll MOD = 1e9 + 7;
+    ll result = 0;
+
+    ll dfs(TreeNode *node) {
+        if (!node)
+            return 0;
+
+        node->val += dfs(node->left) + dfs(node->right);
+        return node->val;
+    }
+
+  public:
+    int maxProduct(TreeNode *root) {
+        ll total = dfs(root);
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode *node = q.front();
+            q.pop();
+
+            if (!node)
+                continue;
+
+            ll curr = (total - node->val) * node->val;
+            updateMax(result, curr);
+
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+
+        return result % MOD;
+    }
+};
+
+int main() {
+    Solution obj;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    println(obj.maxProduct(root));
+}
+
+/*
+Problem: Maximum Product of Splitted Binary Tree
+*/
