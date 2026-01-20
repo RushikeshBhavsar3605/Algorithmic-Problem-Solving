43336861f313c2be2bbdd701539fa181b73b4485
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-20 18:07:10 +0530

    [Day 20] Solved: Construct the Minimum Bitwise Array I

---
+++ daily-challenges/2026/01/20-3314-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    vector<int> minBitwiseArray(vector<int> &nums) {
+        int n = sz(nums);
+        vi ans(n, -1);
+
+        loop(i, 0, n) {
+            loop(num, 0, nums[i])
+                if ((num | (num + 1)) == nums[i]) {
+                    ans[i] = num;
+                    break;
+                }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {2, 3, 5, 7};
+    dump(obj.minBitwiseArray(v));
+    v = {11, 13, 31};
+    dump(obj.minBitwiseArray(v));
+}
+
+/*
+Problem: Construct the Minimum Bitwise Array I
+*/
