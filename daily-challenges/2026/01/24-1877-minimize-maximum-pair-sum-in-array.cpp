991d251ed2c876fd27c286374f658dec0f1cee01
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-24 11:36:50 +0530

    [Day 24] Solved: Minimize Maximum Pair Sum in Array

---
+++ daily-challenges/2026/01/24-1877-minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Sorting + Two Pointer): */
+class Solution {
+  public:
+    int minPairSum(vector<int> &nums) {
+        sort(all(nums));
+        int n = sz(nums), maxi = 0;
+
+        loop(i, 0, (n >> 1))
+            updateMax(maxi, nums[i] + nums[n - i - 1]);
+
+        return maxi;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {3, 5, 2, 3};
+    println(obj.minPairSum(v));
+    v = {3, 5, 4, 2, 4, 6};
+    println(obj.minPairSum(v));
+}
+
+/*
+Problem: Minimize Maximum Pair Sum in Array
+*/
