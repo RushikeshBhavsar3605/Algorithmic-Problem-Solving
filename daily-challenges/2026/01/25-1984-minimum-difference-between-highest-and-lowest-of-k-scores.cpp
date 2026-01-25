9bb4cc039d9a69cf44c257e638031313ef19f6cc
Rushikesh Bhavsar <rvbhavsar36@gmail.com>
2026-01-25 12:13:22 +0530

    [Day 25] Solved: Minimum Difference Between Highest and Lowest of K Scores

---
+++ daily-challenges/2026/01/25-1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long long>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// STL Shortcuts
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define uniq(v)                                                                                                        \
+    sort(all(v));                                                                                                      \
+    v.erase(unique(all(v)), v.end())
+
+// Unordered Containers
+#define umap unordered_map
+#define uset unordered_set
+
+// Loop Macros (readable & consistent)
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define revloop(i, a, b) for (int i = a; i >= b; i--)
+#define iterate(x, a) for (auto &x : a)
+
+// Min/Max Updaters (more explicit names)
+#define updateMin(a, b) (a = min(a, b))
+#define updateMax(a, b) (a = max(a, b))
+
+// Debug Macros (OJ-safe: outputs to cerr)
+#define dump(a)                                                                                                        \
+    do {                                                                                                               \
+        cerr << #a << " = ";                                                                                           \
+        for (auto &x : a)                                                                                              \
+            cerr << x << " ";                                                                                          \
+        cerr << "\n";                                                                                                  \
+    } while (0)
+#define dump2D(a)                                                                                                      \
+    do {                                                                                                               \
+        cerr << #a << ":\n";                                                                                           \
+        for (auto &row : a) {                                                                                          \
+            for (auto &x : row)                                                                                        \
+                cerr << x << " ";                                                                                      \
+            cerr << "\n";                                                                                              \
+        }                                                                                                              \
+    } while (0)
+
+// Printing to stdout (iostream)
+#define print(x)                                                                                                       \
+    do {                                                                                                               \
+        std::cout << (x);                                                                                              \
+    } while (0)
+#define println(x)                                                                                                     \
+    do {                                                                                                               \
+        std::cout << (x) << '\n';                                                                                      \
+    } while (0)
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    int minimumDifference(vector<int> &nums, int k) {
+        sort(all(nums));
+        int mini = INT_MAX;
+
+        for (int left = 0; left + k - 1 < sz(nums); left++) {
+            int right = left + k - 1;
+            updateMin(mini, nums[right] - nums[left]);
+        }
+
+        return mini;
+    }
+};
+
+int main() {
+    Solution obj;
+    vi v = {90};
+    println(obj.minimumDifference(v, 1));
+    v = {9, 4, 1, 7};
+    println(obj.minimumDifference(v, 2));
+}
+
+/*
+Problem: Minimum Difference Between Highest and Lowest of K Scores
+*/
