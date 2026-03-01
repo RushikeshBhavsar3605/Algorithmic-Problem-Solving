bc992f393687e6be6b9058f5999459efb0290be6
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-03-01 13:17:11 +0530

    [Day 1] Solved: Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ daily-challenges/2026/03/01-1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Greedy): */
+class Solution {
+  public:
+    int minPartitions(string n) {
+        int maxi = 0;
+        iterate(it, n)
+            updateMax(maxi, it - '0');
+
+        return maxi;
+    }
+};
+
+int main() {
+    Solution obj;
+    auto res = obj.minPartitions("32");
+    print(res);
+    res = obj.minPartitions("82734");
+    print(res);
+    res = obj.minPartitions("27346209830709182346");
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Partitioning Into Minimum Number Of Deci-Binary Numbers
+*/
