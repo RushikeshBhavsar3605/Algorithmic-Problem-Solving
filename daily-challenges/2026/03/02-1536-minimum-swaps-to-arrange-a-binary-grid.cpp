34571b6171a0d7565f1cd56b6275f21a6e720498
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-03-02 12:42:15 +0530

    [Day 2] Solved: Minimum Swaps to Arrange a Binary Grid

---
+++ daily-challenges/2026/03/02-1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+class Solution {
+  public:
+    int minSwaps(vector<vector<int>> &grid) {
+        int n = sz(grid);
+        vec<int> endZeroes(n, 0);
+
+        loop(i, 0, n) {
+            int j = n - 1;
+
+            int count = 0;
+            while (j >= 0 && grid[i][j] == 0) {
+                count++;
+                j--;
+            }
+
+            endZeroes[i] = count;
+        }
+
+        int steps = 0;
+        loop(i, 0, n) {
+            int need = n - i - 1;
+            int j = i;
+            while (j < n && endZeroes[j] < need)
+                j++;
+
+            if (j == n)
+                return -1;
+
+            steps += j - i;
+            while (j > i) {
+                swap(endZeroes[j], endZeroes[j - 1]);
+                j--;
+            }
+        }
+
+        return steps;
+    }
+};
+
+int main() {
+    Solution obj;
+    vec<vec<int>> v = {{0, 0, 1}, {1, 1, 0}, {1, 0, 0}};
+    auto res = obj.minSwaps(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Minimum Swaps to Arrange a Binary Grid
+*/
