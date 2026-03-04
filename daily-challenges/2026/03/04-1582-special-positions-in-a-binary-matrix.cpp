8d97d3eb267248b72fae0d6daf20a695c90194ad
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-03-04 10:36:52 +0530

    [Day 4] Solved: Special Positions in a Binary Matrix

---
+++ daily-challenges/2026/03/04-1582-special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Hashing): */
+class Solution {
+  public:
+    int numSpecial(vector<vector<int>> &mat) {
+        int n = sz(mat);
+        int m = sz(mat[0]);
+        vec<int> onesInRow(n, 0);
+        vec<int> onesInCol(m, 0);
+
+        loop(i, 0, n)
+            loop(j, 0, m)
+                if (mat[i][j] == 1) {
+                    onesInRow[i]++;
+                    onesInCol[j]++;
+                }
+
+        int res = 0;
+        loop(i, 0, n)
+            loop(j, 0, m)
+                if (mat[i][j] == 1 && onesInRow[i] == 1 && onesInCol[j] == 1)
+                    res++;
+
+        return res;
+    }
+};
+
+int main() {
+    Solution obj;
+    vec<vec<int>> v = {{1, 0, 0}, {0, 0, 1}, {1, 0, 0}};
+    auto res = obj.numSpecial(v);
+    print(res);
+    v = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    res = obj.numSpecial(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Special Positions in a Binary Matrix
+*/
