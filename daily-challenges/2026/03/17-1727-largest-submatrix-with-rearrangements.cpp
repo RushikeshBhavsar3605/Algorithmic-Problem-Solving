6d806388d7952f78faaca290a106e5767fbe0b74
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-03-17 10:24:30 +0530

    [Day 17] Solved: Largest Submatrix With Rearrangements

---
+++ daily-challenges/2026/03/17-1727-largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Precompute): */
+class Solution {
+  public:
+    int largestSubmatrix(vector<vector<int>> &matrix) {
+        int n = sz(matrix), m = sz(matrix[0]);
+        int res = 0;
+        vec<int> prevHeights(m, 0);
+
+        loop(r, 0, n) {
+            vec<int> heights = matrix[r];
+            loop(c, 0, m)
+                if (heights[c] != 0)
+                    heights[c] += prevHeights[c];
+
+            prevHeights = heights;
+
+            sort(all(heights), greater<int>());
+            loop(i, 0, m)
+                updateMax(res, (i + 1) * heights[i]);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution obj;
+    vec<vec<int>> v = {{0, 0, 1}, {1, 1, 1}, {1, 0, 1}};
+    auto res = obj.largestSubmatrix(v);
+    print(res);
+    v = {{1, 0, 1, 0, 1}};
+    res = obj.largestSubmatrix(v);
+    print(res);
+    v = {{1, 1, 0}, {1, 0, 1}};
+    res = obj.largestSubmatrix(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Largest Submatrix With Rearrangements
+*/
