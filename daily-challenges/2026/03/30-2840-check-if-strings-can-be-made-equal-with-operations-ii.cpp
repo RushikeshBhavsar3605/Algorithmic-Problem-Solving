59b6e0b666096df37558365ef6ce11f7d5748869
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-03-31 01:44:58 +0530

    [Day 30] Solved: Check if Strings Can be Made Equal With Operations II

---
+++ daily-challenges/2026/03/30-2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Frequency Array): */
+class Solution {
+  public:
+    bool checkStrings(string s1, string s2) {
+        vec<int> even1(26, 0), odd1(26, 0);
+        vec<int> even2(26, 0), odd2(26, 0);
+
+        loop(i, 0, sz(s1)) {
+            if (i & 1) {
+                odd1[s1[i] - 'a']++;
+                odd2[s2[i] - 'a']++;
+            } else {
+                even1[s1[i] - 'a']++;
+                even2[s2[i] - 'a']++;
+            }
+        }
+
+        return even1 == even2 && odd1 == odd2;
+    }
+};
+
+int main() {
+    Solution obj;
+    auto res = obj.checkStrings("abcdba", "cabdab");
+    print(res);
+    res = obj.checkStrings("abe", "bea");
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Check if Strings Can be Made Equal With Operations II
+*/
