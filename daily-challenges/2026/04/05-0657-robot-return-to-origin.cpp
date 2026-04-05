758d01770ac0ad2e1893fa928aae0086ff3c2b63
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-05 07:55:39 +0530

    [Day 5] Solved: Robot Return to Origin

---
+++ daily-challenges/2026/04/05-0657-robot-return-to-origin.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Naive): */
+class Solution {
+  public:
+    bool judgeCircle(string moves) {
+        int horizontal = 0;
+        int vertical = 0;
+
+        iterate(it, moves) {
+            if (it == 'U')
+                vertical++;
+            if (it == 'D')
+                vertical--;
+            if (it == 'L')
+                horizontal--;
+            if (it == 'R')
+                horizontal++;
+        }
+
+        return horizontal == 0 && vertical == 0;
+    }
+};
+
+int main() {
+    Solution obj;
+    auto res = obj.judgeCircle("UD");
+    print(res);
+    res = obj.judgeCircle("LL");
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Robot Return to Origin
+*/
