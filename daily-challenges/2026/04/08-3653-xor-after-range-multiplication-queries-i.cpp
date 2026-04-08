f0cbfe9e121891ee33d4135f820d73ba05a932e4
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-08 09:58:31 +0530

    [Day 8] Solved: XOR After Range Multiplication Queries I

---
+++ daily-challenges/2026/04/08-3653-xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================
+// Type Aliases
+// =====================
+using ll = long long;
+
+template <typename T> using vec = vector<T>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// =====================
+// STL Helpers
+// =====================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// =====================
+// Unordered Containers
+// =====================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// =====================
+// Loop Macros
+// =====================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// =====================
+// Min / Max Updaters
+// =====================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// =====================
+// Unique (sort + dedup)
+// =====================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// =====================
+// Debug Utilities (OJ-safe)
+// =====================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// =====================
+// Universal Printers
+// =====================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vec<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vec<vec<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Simulation): */
+class Solution {
+  public:
+    int xorAfterQueries(vector<int> &nums, vector<vector<int>> &queries) {
+        int MOD = 1e9 + 7;
+
+        iterate(query, queries) {
+            int li = query[0];
+            int ri = query[1];
+            int ki = query[2];
+            int vi = query[3];
+
+            int idx = li;
+            while (idx <= ri) {
+                nums[idx] = ((ll)nums[idx] * vi) % MOD;
+                idx += ki;
+            }
+        }
+
+        int res = 0;
+        iterate(num, nums)
+            res ^= num;
+
+        return res;
+    }
+};
+
+int main() {
+    Solution obj;
+    vec<int> nums = {1, 1, 1};
+    vec<vec<int>> queries = {{0, 2, 1, 4}};
+    auto res = obj.xorAfterQueries(nums, queries);
+    print(res);
+    nums = {2, 3, 1, 5, 4};
+    queries = {{1, 4, 2, 3}, {0, 2, 1, 2}};
+    res = obj.xorAfterQueries(nums, queries);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: XOR After Range Multiplication Queries I
+*/
