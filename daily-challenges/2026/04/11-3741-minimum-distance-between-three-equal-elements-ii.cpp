015c32030568ba2ddb46c66d1c87ef00f69ecd12
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-11 10:09:08 +0530

    [Day 11] Solved: Minimum Distance Between Three Equal Elements II

---
+++ daily-challenges/2026/04/11-3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Hashing): */
+class Solution {
+  public:
+    int minimumDistance(vector<int> &nums) {
+        umap<int, vi> mp;
+        loop(i, 0, sz(nums))
+            mp[nums[i]].pb(i);
+
+        int res = INT_MAX;
+        iterate(it, mp) {
+            if (sz(it.ss) < 3)
+                continue;
+
+            loop(i, 0, sz(it.ss) - 2) {
+                int abs1 = abs(it.ss[i] - it.ss[i + 1]);
+                int abs2 = abs(it.ss[i + 1] - it.ss[i + 2]);
+                int abs3 = abs(it.ss[i + 2] - it.ss[i]);
+                updateMin(res, abs1 + abs2 + abs3);
+            }
+        }
+
+        return res != INT_MAX ? res : -1;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {1, 2, 1, 1, 3};
+    auto res = obj.minimumDistance(v);
+    print(res);
+    v = {1, 1, 2, 3, 2, 1, 2};
+    res = obj.minimumDistance(v);
+    print(res);
+    v = {1};
+    res = obj.minimumDistance(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Minimum Distance Between Three Equal Elements II
+*/
