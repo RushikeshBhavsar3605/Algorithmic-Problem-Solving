08c82e20b9604b3598d4554478eddb4d5e8b7e9b
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-16 11:47:37 +0530

    [Day 16] Solved: Closest Equal Element Queries

---
+++ daily-challenges/2026/04/16-3488-closest-equal-element-queries.cpp
@@ -0,0 +1,182 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Hashing): */
+class Solution {
+  public:
+    vector<int> solveQueries(vector<int> &nums, vector<int> &queries) {
+        int n = sz(nums);
+        vi left(n), right(n);
+        umap<int, int> pos;
+
+        loop(i, -n, n) {
+            if (i >= 0)
+                left[i] = pos[nums[i]];
+
+            pos[nums[(i + n) % n]] = i;
+        }
+        pos.clear();
+
+        revloop(i, 2 * n - 1, 0) {
+            if (i < n)
+                right[i] = pos[nums[i]];
+
+            pos[nums[i % n]] = i;
+        }
+
+        int m = sz(queries);
+        vi res(m);
+        loop(i, 0, m) {
+            int x = queries[i];
+
+            if (x - left[x] == n)
+                res[i] = -1;
+            else
+                res[i] = min(x - left[x], right[x] - x);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {1, 3, 1, 4, 1, 3, 2};
+    vi q = {0, 3, 5};
+    auto res = obj.solveQueries(v, q);
+    print(res);
+    v = {1, 2, 3, 4};
+    q = {0, 1, 2, 3};
+    res = obj.solveQueries(v, q);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Closest Equal Element Queries
+*/
