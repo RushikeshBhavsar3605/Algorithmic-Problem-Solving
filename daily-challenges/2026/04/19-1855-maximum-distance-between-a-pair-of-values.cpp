3b260d5ea1666eae9dbfcc30bd94c2fc9159382f
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-19 11:37:49 +0530

    [Day 19] Solved: Maximum Distance Between a Pair of Values

---
+++ daily-challenges/2026/04/19-1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Two Pointer): */
+class Solution {
+  public:
+    int maxDistance(vector<int> &nums1, vector<int> &nums2) {
+        int idx1 = sz(nums1) - 1;
+        int idx2 = sz(nums2) - 1;
+        int res = 0;
+
+        while (idx1 >= 0 && idx2 >= 0) {
+            if (idx1 > idx2)
+                idx1--;
+            else if (nums1[idx1] > nums2[idx2])
+                idx2--;
+            else {
+                updateMax(res, idx2 - idx1);
+                idx1--;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi nums1 = {55, 30, 5, 4, 2};
+    vi nums2 = {100, 20, 10, 10, 5};
+    auto res = obj.maxDistance(nums1, nums2);
+    print(res);
+    nums1 = {2, 2, 2};
+    nums2 = {10, 10, 1};
+    res = obj.maxDistance(nums1, nums2);
+    print(res);
+    nums1 = {30, 29, 19, 5};
+    nums2 = {25, 25, 25, 25, 25};
+    res = obj.maxDistance(nums1, nums2);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Maximum Distance Between a Pair of Values
+*/
