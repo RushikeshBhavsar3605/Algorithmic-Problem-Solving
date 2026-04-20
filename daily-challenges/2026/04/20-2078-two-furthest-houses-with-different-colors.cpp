06fadd93e6ae158aef72e39cc5280e116e0b3ca6
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-04-20 11:19:36 +0530

    [Day 20] Solved: Two Furthest Houses With Different Colors

---
+++ daily-challenges/2026/04/20-2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach 1 (Naive):
+class Solution {
+  public:
+    int maxDistance(vector<int> &colors) {
+        int n = sz(colors);
+        int maxi = 0;
+
+        loop(i, 0, n) {
+            loop(j, i, n) {
+                if (colors[i] != colors[j])
+                    updateMax(maxi, j - i);
+            }
+        }
+
+        return maxi;
+    }
+};
+*/
+
+/* Approach 2 (Two Pointer): */
+class Solution {
+  public:
+    int maxDistance(vector<int> &colors) {
+        int n = sz(colors);
+        int left = 0, right = 0;
+
+        loop(i, 0, n)
+            if (colors[i] ^ colors[n - 1]) {
+                left = i;
+                break;
+            }
+
+        revloop(i, n - 1, 0)
+            if (colors[i] ^ colors[0]) {
+                right = i;
+                break;
+            }
+
+        return max(n - 1 - left, right);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {1, 1, 1, 6, 1, 1, 1};
+    auto res = obj.maxDistance(v);
+    print(res);
+    v = {1, 8, 3, 8, 3};
+    res = obj.maxDistance(v);
+    print(res);
+    v = {0, 1};
+    res = obj.maxDistance(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Two Furthest Houses With Different Colors
+*/
