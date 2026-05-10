5d2a43cfadcbddad6ac29828ed574f6cc27d1e45
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-05-11 00:29:35 +0530

    [Day 10] Solved: Maximum Number of Jumps to Reach the Last Index

---
+++ daily-challenges/2026/05/10-2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,206 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach (Memoization):
+class Solution {
+  private:
+    int solve(int i, vi &nums, int target, vi &memo) {
+        int n = sz(nums);
+
+        if (i == n - 1)
+            return 0;
+
+        if (memo[i] != -1)
+            return memo[i];
+
+        int res = INT_MIN;
+        loop(j, i + 1, n)
+            if (abs(nums[j] - nums[i]) <= target) {
+                int next = solve(j, nums, target, memo);
+
+                if (next != INT_MIN)
+                    updateMax(res, next + 1);
+            }
+
+        return memo[i] = res;
+    }
+
+  public:
+    int maximumJumps(vector<int> &nums, int target) {
+        int n = sz(nums);
+        vi memo(n, -1);
+        int res = solve(0, nums, target, memo);
+
+        return res != INT_MIN ? res : -1;
+    }
+};
+*/
+
+/* Approach 2 (Tabulation): */
+class Solution {
+  public:
+    int maximumJumps(vector<int> &nums, int target) {
+        int n = sz(nums);
+        vi dp(n + 1, 0);
+
+        revloop(i, n - 1, 0) {
+            int res = INT_MIN;
+
+            loop(j, i + 1, n) {
+                if (abs(nums[j] - nums[i]) <= target) {
+                    int next = dp[j];
+
+                    if (next != INT_MIN)
+                        updateMax(res, next + 1);
+                }
+
+                dp[i] = res;
+            }
+        }
+
+        return dp[0] != INT_MIN ? dp[0] : -1;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {1, 3, 6, 4, 1, 2};
+    auto res = obj.maximumJumps(v, 2);
+    print(res);
+    v = {1, 3, 6, 4, 1, 2};
+    res = obj.maximumJumps(v, 3);
+    print(res);
+    v = {1, 3, 6, 4, 1, 2};
+    res = obj.maximumJumps(v, 0);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Maximum Number of Jumps to Reach the Last Index
+*/
