301e8acfacf039099470c5ba1461e397cd22f600
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-05-18 23:44:29 +0530

    [TOP-150] Solved: Sliding Window problems

---
+++ top-150/03-sliding-window/01-0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+/* Approach (Sliding Window): */
+class Solution {
+  public:
+    int minSubArrayLen(int target, vector<int> &nums) {
+        int n = sz(nums);
+        int length = INT_MAX;
+        int left = 0, right = 0;
+        int currSum = 0;
+
+        while (right < n) {
+            currSum += nums[right];
+
+            while (currSum >= target) {
+                updateMin(length, right - left + 1);
+                currSum -= nums[left];
+                left++;
+            }
+
+            right++;
+        }
+
+        return length != INT_MAX ? length : 0;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {2, 3, 1, 2, 4, 3};
+    auto res = obj.minSubArrayLen(7, v);
+    print(res);
+    v = {1, 4, 4};
+    res = obj.minSubArrayLen(4, v);
+    print(res);
+    v = {1, 1, 1, 1, 1, 1, 1, 1};
+    res = obj.minSubArrayLen(11, v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Minimum Size Subarray Sum
+*/
