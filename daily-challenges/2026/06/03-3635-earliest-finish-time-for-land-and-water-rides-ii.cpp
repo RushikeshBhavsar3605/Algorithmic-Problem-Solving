4522b185f9f9da72bd0c666a0e9e722f45a3b7d6
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-06-04 23:10:17 +0530

    [Day 3] Solved: Earliest Finish Time for Land and Water Rides II

---
+++ daily-challenges/2026/06/03-3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ==========================================
+// Type Aliases
+// ==========================================
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+// N-dimensional vector
+template <typename T, int N> struct nvec {
+    using type = vector<typename nvec<T, N - 1>::type>;
+};
+
+template <typename T> struct nvec<T, 1> {
+    using type = vector<T>;
+};
+
+template <typename T, int N> using nvec_t = typename nvec<T, N>::type;
+
+// ==========================================
+// Pair Helpers
+// ==========================================
+#define ff first
+#define ss second
+
+// ==========================================
+// STL Helpers
+// ==========================================
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+template <typename T> inline int sz(const T &x) { return static_cast<int>(x.size()); }
+
+// ==========================================
+// Unordered Containers
+// ==========================================
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+template <typename T> using uset = unordered_set<T>;
+
+// ==========================================
+// Loop Macros
+// ==========================================
+#define loop(i, a, b) for (auto i = (a); i < (b); i++)
+#define loop_le(i, a, b) for (auto i = (a); i <= (b); i++)
+#define revloop(i, a, b) for (auto i = (a); i >= (b); i--)
+#define iterate(x, a) for (auto &x : (a))
+
+// ==========================================
+// Min / Max Updaters
+// ==========================================
+template <typename T> inline void updateMin(T &a, const T &b) {
+    if (b < a)
+        a = b;
+}
+
+template <typename T> inline void updateMax(T &a, const T &b) {
+    if (b > a)
+        a = b;
+}
+
+// ==========================================
+// Unique (sort + dedup)
+// ==========================================
+template <typename T> inline void uniq(T &v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+// ==========================================
+// Debug Utilities (OJ-safe)
+// ==========================================
+#define LOCAL
+#ifdef LOCAL
+
+template <typename T> void dump(const T &a, const char *name) {
+    cerr << name << " = ";
+    for (const auto &x : a)
+        cerr << x << ' ';
+    cerr << '\n';
+}
+
+template <typename T> void dump2D(const T &a, const char *name) {
+    cerr << name << ":\n";
+    for (const auto &row : a) {
+        for (const auto &x : row)
+            cerr << x << ' ';
+        cerr << '\n';
+    }
+}
+
+#define DUMP(x) dump(x, #x)
+#define DUMP2D(x) dump2D(x, #x)
+
+#else
+#define DUMP(x)
+#define DUMP2D(x)
+#endif
+
+// ==========================================
+// Universal Printers
+// ==========================================
+template <typename T> void print(T x) { cout << x << endl; }
+
+template <typename T> void print(vector<T> &v) {
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <typename T> void print(vector<vector<T>> &vv) {
+    for (auto &v : vv) {
+        for (auto &x : v)
+            cout << x << " ";
+        cout << endl;
+    }
+}
+
+template <typename Node> void print(Node *head) {
+    Node *curr = head;
+    while (curr) {
+        cout << curr->val;
+        if (curr->next)
+            cout << " -> ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+class Solution {
+  private:
+    int solve(vi &start1, vi &duration1, vi &start2, vi &duration2) {
+        int finish1 = INT_MAX;
+        loop(i, 0, sz(start1))
+            updateMin(finish1, start1[i] + duration1[i]);
+
+        int finish2 = INT_MAX;
+        loop(i, 0, sz(start2))
+            updateMin(finish2, max(start2[i], finish1) + duration2[i]);
+
+        return finish2;
+    }
+
+  public:
+    int earliestFinishTime(vector<int> &landStartTime, vector<int> &landDuration, vector<int> &waterStartTime,
+                           vector<int> &waterDuration) {
+        int landWater = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterLand = solve(waterStartTime, waterDuration, landStartTime, landDuration);
+
+        return min(landWater, waterLand);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi ls = {2, 8}, ld = {4, 1}, ws = {6}, wd = {3};
+    auto res = obj.earliestFinishTime(ls, ld, ws, wd);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Earliest Finish Time for Land and Water Rides II
+*/
