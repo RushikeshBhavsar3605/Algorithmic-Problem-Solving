1589b325874b0d6547d82527e4772d28cbb14d78
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-06-20 10:42:00 +0530

    [Day 19] Solved: Find the Highest Altitude

---
+++ daily-challenges/2026/06/19-1732-find-the-highest-altitude.cpp
@@ -0,0 +1,35 @@
+#include "template.hpp"
+
+/* Approach (Naive): */
+class Solution {
+  public:
+    int largestAltitude(vector<int> &gain) {
+        int maxi = 0;
+        int curr = 0;
+
+        iterate(it, gain) {
+            curr += it;
+            updateMax(maxi, curr);
+        }
+
+        return maxi;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution obj;
+    vi v = {-5, 1, 5, 0, -7};
+    auto res = obj.largestAltitude(v);
+    print(res);
+    v = {-4, -3, -2, -1, 4, 3, 2};
+    res = obj.largestAltitude(v);
+    print(res);
+    return 0;
+}
+
+/*
+Problem: Find the Highest Altitude
+*/
